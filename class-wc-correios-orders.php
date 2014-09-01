c3225bd5951179b31cbcfe6d86d7b0672b52098f
Anderson Narciso <anderson@agenciawebsul.com.br>
2014-09-01 01:07:43 -0300

    Update class-wc-correios-orders.php

--- class-wc-correios-orders.php
+++ class-wc-correios-orders.php
@@ -149,10 +149,10 @@ class WC_Correios_Orders {
 
 		if ( ! empty( $tracking_code ) ) {
 			$url = sprintf( '<a href="http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=%1$s" target="_blank">%1$s</a>', $tracking_code );
-			echo '<h4>Rastramento de sua compra!</h4>';
+		
       echo '<div class="woocommerce-info"><strong>' . __( 'Correios', 'woocommerce-correios' ) . ':</strong> ' . sprintf( __( 'Your the tracking code: %s.', 'woocommerce-correios' ), $url ) . '</div>';
       echo '<br />';
-    
+    	echo '<h4>Rastramento de sua compra!</h4>';
     // Function Trace table of posts for the plugin Woocommerce Correios 
     
     function tabelaDeRastreamento($tracking_code){
